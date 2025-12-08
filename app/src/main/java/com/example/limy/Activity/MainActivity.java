cdb837063f0e576e7ae0437d2df27db3697ad09a
GODOT <arthurrenan555@gmail.com>
2025-12-08 12:14:24 -0300

    a

---
+++ app/src/main/java/com/example/limy/Activity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.limy.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.limy.Adapter.PopularAdapter;
+import com.example.limy.databinding.ActivityMainBinding;
+import com.example.limy.domain.PopularDomain;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        initRecyclerview();
+    }
+
+    private void initRecyclerview() {
+        ArrayList<PopularDomain> items=new ArrayList<>();
+        items.add(new PopularDomain("Tomates","item_1",15,4,500,"test"));
+        items.add(new PopularDomain("Leite","item_2",10,4.5,450,"test"));
+        items.add(new PopularDomain("Batatas","item_3",3,4.9,800,"test"));
+
+        binding.PopularView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
+        binding.PopularView.setAdapter(new PopularAdapter(items));
+    }
+}
